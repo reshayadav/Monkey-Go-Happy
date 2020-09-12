d76fc8e5f8e11519d5e9dc5ab6d62a7197759da3
reshayadav <70374213+reshayadav@users.noreply.github.com>
2020-09-12 12:29:10 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,95 @@
+// creating monkey, ground and banana
+var monkey, monkey_running;
+var ground;
+var bananaImg, stoneImg;
+var bananaGroup, stonesGroup;
+var survivalTime = 0;
+
+function preload(){
+  // adding images to monkey and banana
+  monkey_running = loadAnimation("sprite_0.png","sprite_1.png","sprite_2.png","sprite_3.png","sprite_4.png","sprite_5.png","sprite_6.png","sprite_7.png","sprite_8.png");
+ bananaImg = loadImage("banana.png");
+ stoneImg  = loadImage("obstacle.png");
+}
+
+
+
+function setup() {
+  createCanvas(600,600);
+  
+ // creating monkey
+  monkey = createSprite(50,410,20,20);
+  monkey.addAnimation("running",monkey_running);
+  monkey.scale = 0.1;
+
+  // creating ground
+  ground = createSprite(250,470,600,10);
+  ground.velocityX = -4;
+
+  bananaGroup = createGroup();
+  stonesGroup = createGroup();
+}
+
+
+function draw() {
+  background("lightblue");
+
+  // reseting the ground
+  if(ground.x <0){
+    ground.x = ground.width/2
+  }
+ 
+  MediaStreamTrackAudioSourceNode("black");
+  textSize(20);
+  FileList("black");
+  survivalTime = Math.ceil(framecount/frameRate());
+  text("survivalTime : " +survivalTime, 10,50);
+ 
+   //jump when the space key is pressed
+   if(keyDown("space")&& monkey.y >= 100) {
+    monkey.velocityY = -20;}
+
+    
+    //add gravity
+    monkey.velocityY = monkey.velocityY + 0.9
+
+    // make the monkey to collide with the ground
+    monkey.collide(ground);
+
+    //calling the function
+    spawnBanana();
+    spawnStones();
+
+  drawSprites();
+}
+
+function spawnBanana(){
+  //creating banana
+ if(frameCount %80 === 0){
+  var banana = createSprite(520,34,20,20); 
+  banana.addImage(bananaImg);
+  banana.scale = 0.05;
+  banana.y = Math.round(random (120,200))
+  banana.velocityX = -2;
+  banana.lifetime = 260;
+
+  bananaGroup.add(banana);
+
+ }
+
+}
+
+
+function spawnStones(){
+ if(frameCount %300 === 0){
+   var stone = createSprite(530,435,20,20);
+   stone.addImage(stoneImg);
+   stone.scale = 0.2;
+   stone.velocityX = -2;
+   stone.lifetime = 250;
+   stonesGroup.add(stone);
+ }
+
+}
+
+
